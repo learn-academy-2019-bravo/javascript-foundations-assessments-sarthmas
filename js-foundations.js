c2b4f48478be1ebb1f4647da633623809ca1bfe9
sarthmas <sarah@Julias-Air.localdomain>
2019-04-26 15:25:04 -0700

    Submitting Assessment 2 for Sarah Thomas

--- js-foundations.js
+++ js-foundations.js
@@ -1,14 +1,56 @@
 // Javascript Foundations Assessments
 
-// 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three" or, print the number and "is not divisble by three".
+/* 1. Write an anonymous function that takes one argument of type number and
+decides if that number is evenly divisble by three or not. If it is, print the number and
+"is divisible by three" or, print the number and "is not divisble by three".*/
 
-// 2. Write an object called helloMe. Include your first_name, last_name and at least two other properties of you. Write a function that returns a description of you.
+function divThree(num){
+  if (num % 3 === 0) {
+    return (num + " is divisible by three.")
+  } else {
+    return (num + " is not divisble by three.")
+  }
+}
+console.log(divThree(12))
 
-// 3. Create an array of 5 grocery items. Write a function that returns the first, middle, and last item as a string.
+/* 2. Write an object called helloMe. Include your first_name, last_name and at
+least two other properties of you. Write a function that returns a description of you.*/
 
-// 4. Write a function called alphabetSoup that takes an argument of "str" and return a string with the letters in alphabetical order (ie. learn becomes aelnr). Assume numbers and punctuation symbols will not be included in the parameter.
+var helloMe = { first_name: "Sarah",
+                last_name: "Thomas",
+                hair_color: "brown",
+                eye_color: "brown" ,
+                descriptionMe: function() { return "First Name: " + this.first_name + ", Last Name: " + this.last_name + ", Hair Color: " + this.hair_color + ", Eye Color: " + this.hair_color}
+                };
 
-// 5. Given the arrays below, write a function animalNums that uses a for loop to print one value from each array concatenated together.
+    console.log (helloMe.descriptionMe());
+
+
+
+/* 3. Create an array of 5 grocery items. Write a function that returns the first,
+middle, and last item as a string.*/
+
+const items = ["apple", "pear", "orange", "milk", "butter"]
+
+function grocery(items){
+  return (items[0] + items[2] + items[4])
+}
+  console.log(grocery(items));
+
+/* 4. Write a function called alphabetSoup that takes an argument of "str" and
+return a string with the letters in alphabetical order (ie. learn becomes aelnr).
+Assume numbers and punctuation symbols will not be included in the parameter.*/
+
+function alphabetSoup(str){
+  var strArray = str.split("")
+  strArray.sort()
+  return strArray.join("")
+  }
+
+console.log(alphabetSoup("hello"));
+
+/* 5. Given the arrays below, write a function animalNums that uses a for loop
+to print one value from each array concatenated together.*/
 
 var nums = [9, 5, 88, 2, 5, 42, 57]
 
@@ -16,4 +58,27 @@ var nouns = ["ducks", "elephants", "pangolin", "rhinoceros", "giraffes", "pengui
 
 // output should be: "9 ducks", etc
 
-//6. Create an array of 5 numbers. Write a function that loops through the array and returns a new array of the numbers multiplied by 5.
+function animalNums(nums,nouns){
+  let newAr = []
+  for(i=0; i < nums.length; i++){
+    newAr.push(nums[i] + " " + nouns[i])
+  }
+  return newAr
+}
+
+console.log(animalNums(nums,nouns));
+
+/*6. Create an array of 5 numbers. Write a function that loops
+through the array and returns a new array of the numbers multiplied by 5.*/
+
+var arr1 = [1, 5, 7, 3, 10]
+
+function mult5(array){
+    let newArr = []
+    for(let i = 0; i < array.length; i++){
+        newArr.push(array[i] * 5)
+    }
+    return newArr
+}
+
+console.log(mult5(arr1));
--- js-foundations.md
+++ js-foundations.md
@@ -4,23 +4,44 @@
 
 1. List the data types in Javascript. Indicate which are primitives.
 
+Data types: string, number, boolean, null, undefined, symbol, object
+All are primitive except object.
+
 2. What's the difference between =, ==, and === in JavaScript?
 
+Strict equality (===) means values which we are comparing must have the same type. This means "2" will not be equal to 2 ("2"===2 it will return false)
+
+Type converting equality (==) means automatically it will covert the variable to value irrespective of data type; either it is a string or a number.
+
+One equal sign (=) is an arithmetic operator used to calculate values. Arithmetic operators are used to perform arithmetic between variables and/or values.
+
 3. What is the difference between an array's index and length?
+An array's index is the set of numbers indicating the position of each character in the array.  The first item in the index is always at index number zero.  The length of an array is the number of items in the array.  The length of an array will be equal to the total number of items in the index plus one (because index starts at zero and length starts at one).
 
 4. What are the three parts of a for loop?
+Java for loop statement has three parts: initialization sets a loop control variable to an initial value. condition is a Boolean expression that tests the loop control variable. The third part is the iteration through the loop.
 
 5. What is a function's declaration, argument, and call?
+A function's declaration defines a function's name, return type, and parameters.
+A function's argument is the variable passed into the function.
+Calling a function is running it with the specified variable in it and getting the intended result to occur.
 
 6. What are the three main steps in saving work to github?
+git add .
+git commit -m"message"
+git push
 
 7. What is the terminal command to move directories?
+cd
 
 8. Do you have a suggestion for a Check In question?
+Do you prefer mac or PC?  
 
 9. What was your favorite topic this week?
+Discussion of JSON - starting to understand this database language and how information can be pulled from APIs.  I liked the lecture on how the internet works.
 
 10. What was your "A-ha!" moment this week?
+APIs are generally all written in JSON which makes the data compatible for aggregation and use in other applications.
 
 ### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
 
